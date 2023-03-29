f2e341560fcd33d3ac716473a32b41584f9c4047
jowell <87069318+JoelDigbeu@users.noreply.github.com>
2023-03-29 10:33:46 +0000

    class learning

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+class Student {
+  constructor(private name: string, private age: number) {}
+
+  sayHello() {
+    return `
+    Hello ! I'm ${this.name} 
+    and i've ${this.age} year${this.age <= 1 ? '' : 's'} old
+    `
+  }
+}
+
+const baby = new Student('Baby', 1)
+const jowell = new Student('Jowell', 24)
+const lucas = new Student('Lucas', 28)
+
+console.log(baby.sayHello())
+console.log(jowell.sayHello())
+console.log(lucas.sayHello())
